936f9616f822728d5493dc247c7fbba957d74507
Binit Gajera <binit_gajera@yahoo.com>
2019-11-23 19:03:01 -0500

    Just printing and init
    Just creating a pretty table and initializing the for loop for all instruction objects

--- MIPS.py
+++ MIPS.py
@@ -5,127 +5,90 @@
 
 from load_data import Init_MIPS
 from pprint import pprint as pp
+from prettytable import PrettyTable
+    
+class Instructions:
+    def __init__(self, instruction, config):
+        self.IF = 0
+        self.ID = 0
+        self.EX = 0
+        self.WB = 0
+        self.RAW = 'N'
+        self.WAR = 'N'
+        self.WAW = 'N'
+        self.Struct = 'N'
+        self.stat = 0
+        self.name = instruction[0]
+        if len(instruction)>1:
+            self.op1 = instruction[1]
+            self.op2 = instruction[2]
+            self.op3 = None if len(instruction[1:]) == 2 else instruction[3]
+        else:
+            self.op1 = None
+            self.op2 = None
+            self.op3 = None
+        if self.name in ["HLT", "J"]:
+            self.cycles = 0
+            self.pip = None
+        elif self.name in ["BEQ", "BNE"]:
+            self.cycles = 0
+            self.pip = None
+        elif self.name in ["DADD", "DADDI", "DSUB", "DSUBI", "AND", "ANDI", "OR", "ORI"]:
+            self.cycles = 2
+            self.pip = None
+        elif self.name in ["LW", "SW", "L.D", "S.D"]:
+            self.cycles = 1 + config["MEM"]
+            self.pip = None
+        elif self.name in ["ADD.D", "SUB.D"]:
+            self.cycles = config["ADD"][0]
+            self.pip = config["ADD"][1]
+        elif self.name in ["MUL.D"]:
+            self.cycles = config["MULT"][0]
+            self.pip = config["MULT"][1]
+        elif self.name in ["DIV.D"]:
+            self.cycles = config["DIV"][0]
+            self.pip = config["DIV"][1]
+            
+class Processor:
+    def __init__(self, instructions):
+        self.incomplete = True
+        self.IFbusy, self.IDbusy, self.EXbusy, self.MEMbusy, self.WBbusy = 0, 0, 0, 0, 0
+        self.reg_stat = dict()
+        self.IU, self.ADD, self.MULT, self.DIV = 0, 0, 0, 0
+        self.run(instructions)
+                
+    def run(self, instructions):
+        idx = len(instructions)
+        i = 0
+        while i<idx:
+            for inst in instructions:
+                pass
+            i+=1
 
-IF, ID, EX, WB = 0, 0, 0, 0
-RAW, WAR, WAW, Struct = "N", "N", "N", "N"
+def print_table(inst, b):
+    x = PrettyTable()
 
-result = list()
-
-def processor(mips):
-    insts, config, reg, loops, data = mips.instructions, mips.config_dict, mips.reg_val, mips.loops, mips.data
-    global IF, ID, EX, WB, RAW, WAR, WAW, Struct, result
-    reg_stat = dict()    
-    mem_stat = 0
-    for inst in insts[:11]:
-        if inst[0] == "L.D":
-            n_cycles = config["MEM"] + 1                        
-            IF = update_IF(IF, ID)
-###########-Verify this logic for RAW-###########
-            ID, RAW = update_ID("IU", reg_stat, IF, inst)
-###########-###########-###########-#############
-            stalls = mem_stat - (ID + n_cycles - config["MEM"])     
-            EX, mem_stat, Struct = update_EX("IU", mem_stat, ID, n_cycles, stalls)
-            WB = EX + 1
-            reg_stat[inst[1]] = WB
-        elif inst[0] == "ADD.D" or inst[0] == "SUB.D":
-            n_cycles = config["ADD"][0]
-            pip = config["ADD"][1]
-            IF = update_IF(IF, ID)
-###########-Verify this logic for RAW-###########
-            ID, RAW = update_ID("MATH", reg_stat, IF, inst)
-###########-###########-###########-#############    
-            if pip:
-###########-Modify this logic for pipeline-###########                
-                EX = ID + n_cycles                
-###########-###########-###########-##################
-            else:
-                EX = ID + n_cycles
-            WB = EX + 1
-            reg_stat[inst[1]] = WB
-            Struct = "N"
-        elif inst[0] == "MUL.D":
-            n_cycles = config["MULT"][0]
-            pip = config["MULT"][1]
-            IF = update_IF(IF, ID)
-###########-Verify this logic for RAW-###########
-            ID, RAW = update_ID("MATH", reg_stat, IF, inst)
-###########-###########-###########-#############    
-            if pip:
-###########-Modify this logic for pipeline-###########                
-                EX = ID + n_cycles                
-###########-###########-###########-##################
-            else:
-                EX = ID + n_cycles
-            WB = EX + 1
-            reg_stat[inst[1]] = WB
-            Struct = "N"
-        elif inst[0] == "DADDI" or inst[0] == "DSUB":
-            n_cycles = config["MEM"]            
-            IF = update_IF(IF, ID)
-###########-Verify this logic for RAW-###########
-            ID, RAW = update_ID("IU", reg_stat, IF, inst)
-###########-###########-###########-#############                        
-            stalls = mem_stat - (ID + n_cycles - config["MEM"])     
-            EX, mem_stat, Struct = update_EX("IU", mem_stat, ID, n_cycles, stalls)
-            WB = EX + 1
-            reg_stat[inst[1]] = WB
-        elif inst[0] == "BNE":
-            IF = update_IF(IF, ID)
-###########-Verify this logic for RAW-###########
-            ID, RAW = update_ID("IU", reg_stat, IF, inst)
-###########-###########-###########-#############
-            EX, WB = "-","-"
-            WAR, WAW, Struct = "N","N","N"
-        result.append([IF, ID, EX, WB, RAW, WAR, WAW, Struct])
+    x.field_names = ["Instruction","FT", "ID", "EX", "WB", "RAW", "WAR", "WAW", "Struct"]
+    
+    for i, el in enumerate(instructionObjs):
+        if len(b[i]) == 1:
+            tmp = b[i][0]
+        elif len(b[i]) == 3:
+            tmp = b[i][0]+" "+b[i][1]+", "+b[i][2]
+        else:
+            tmp = b[i][0]+" "+b[i][1]+", "+b[i][2]+", "+b[i][3]
+        x.add_row([tmp,el.IF, el.ID, el.EX, el.WB, el.RAW, el.WAR, el.WAW, el.Struct])
         
-def update_IF(IF, ID):
-    res = 0 
-    if IF == 0:
-        res = IF + 1
-    else:
-        res = ID
-    
-    return res
+    return x
 
-def update_ID(iname, reg_st, IF, inst):
-    if iname == "IU":
-        if reg_st.get(inst[1], 0) > IF + 1:
-            ID = reg_st[inst[1]]
-            RAW = "Y"
-        else:
-            ID = IF + 1
-            RAW = "N"
-        return ID, RAW
-    elif iname == "MATH":
-        if reg_st.get(inst[2], 0) > IF + 1 and reg_st.get(inst[3], 0) > IF + 1:
-                ID = max(reg_st[inst[2]], reg_st[inst[3]])
-                RAW = "Y"
-        elif reg_st.get(inst[2], 0) > IF + 1:                
-            ID = reg_st[inst[2]]
-            RAW = "Y"
-        elif reg_st.get(inst[3], 0) > IF + 1:                
-            ID = reg_st[inst[3]]
-            RAW = "Y"
-        else:
-            ID = IF + 1
-            RAW = "N"
-        return ID, RAW
-
-def update_EX(iname, mem_st, ID, n_c, sts, pip=False):
-    if iname == "IU":
-        if sts > 0:
-            EX = ID + n_c + sts
-            mem_st = EX
-            Struct = "Y"
-        else:
-            EX = ID + n_c
-            mem_st = EX
-            Struct = "N"
-        return EX, mem_st, Struct
-    
 if __name__ == "__main__" :
     init_mips = Init_MIPS("./config.txt", "./inst.txt", "./data.txt", "./reg.txt")
     d = init_mips.config_dict
     b = init_mips.instructions
-    processor(init_mips)
-    pp(result)    
+    instructionObjs = list()
+    for el in init_mips.instructions:
+        instructionObjs.append(Instructions(el, d))
+    mips = Processor(instructionObjs)
+    
+    print(print_table(instructionObjs, b))
+    
